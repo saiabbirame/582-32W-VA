4587d6782835e1050518d8a753a2bcad352b2e1e
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-08-03 18:44:57 -0400

    Create Place model

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import db, User
+from models import db, User, Place
 
 app = Flask(__name__)
 
--- code/models.py
+++ code/models.py
@@ -41,3 +41,60 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return f"<User {self.id}: {self.email}>"
+    
+
+class Place(db.Model):
+    __tablename__ = "place"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.String(120),
+        nullable=False
+    )
+
+    category = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    neighbourhood = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    address = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+    description = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    opening_hours = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False
+    )
+
+    latitude = db.Column(
+        db.Float,
+        nullable=False
+    )
+
+    longitude = db.Column(
+        db.Float,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f"<Place {self.id}: {self.name}>"
