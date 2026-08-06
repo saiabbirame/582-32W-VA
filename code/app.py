91bd4acf9e1c4ea2301ececd33ebd7f0d8a4f3b6
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-08-06 00:46:27 -0400

    Accept POST requests on login route

--- code/app.py
+++ code/app.py
@@ -115,8 +115,11 @@ def register():
 
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        pass
+
     return render_template("login.html")
 
 @app.route("/")
