d1087548769882c7e2914b9619a5cc0d575c36fd
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-08-19 10:42:33 -0400

    Add map feature to place details page

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -1,6 +1,24 @@
+// NAVIGATION
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('open');
 });
+
+// MAP
+const mapContainer = document.querySelector('#map');
+
+if (mapContainer) {
+    const latitude = mapContainer.dataset.latitude;
+    const longitude = mapContainer.dataset.longitude;
+
+    const map = L.map('map').setView([latitude, longitude], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    const marker = L.marker([latitude, longitude]).addTo(map);
+}
